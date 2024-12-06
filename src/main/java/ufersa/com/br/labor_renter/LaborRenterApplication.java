e1686c2d900cecea5c1149024c4c071f378eb252
Artur Segantini Guedes <artur.guedes@alunos.ufersa.edu.br>
2024-12-06 16:32:59 -0300

    Initial Commit

---
+++ src/main/java/ufersa/com/br/labor_renter/LaborRenterApplication.java
@@ -0,0 +1,13 @@
+package ufersa.com.br.labor_renter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborRenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborRenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufersa/com/br/labor_renter/LaborRenterApplicationTests.java
@@ -0,0 +1,13 @@
+package ufersa.com.br.labor_renter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborRenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
