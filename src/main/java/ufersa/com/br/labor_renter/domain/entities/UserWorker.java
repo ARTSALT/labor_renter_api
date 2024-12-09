1f76e97564cf7d19e59f26c0defba75a47a8eccf
Ricardo <ricardomarinho638@gmail.com>
2024-12-09 09:04:30 -0300

    userworker repository created

--- src/main/java/ufersa/com/br/labor_renter/domain/entities/UserWorker.java
+++ src/main/java/ufersa/com/br/labor_renter/domain/entities/UserWorker.java
@@ -1,9 +1,13 @@
 package ufersa.com.br.labor_renter.domain.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_user_worker")
 public class UserWorker extends UserAbstract {
+    @Column(nullable = false)
     private String documento;
 
     public String getDocumento() {
