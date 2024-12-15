af365fb6f4b30980927eaba043be8c1ce37fc1a3
Ricardo <ricardomarinho638@gmail.com>
2024-12-15 20:22:22 -0300

    contract create dto

---
+++ src/main/java/ufersa/com/br/labor_renter/api/dto/requests/ContractCreateRequest.java
@@ -0,0 +1,29 @@
+package ufersa.com.br.labor_renter.api.dto.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ufersa.com.br.labor_renter.domain.entities.Contract;
+import ufersa.com.br.labor_renter.domain.entities.Contractor;
+import ufersa.com.br.labor_renter.domain.entities.Job;
+import ufersa.com.br.labor_renter.domain.entities.UserWorker;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContractCreateRequest {
+
+    @NotBlank(message = "O campo é obrigatório")
+    private Long userWorkerId;
+
+    @NotBlank(message = "O campo trabalho é obrigatório")
+    private Long jobId;
+
+    public ContractCreateRequest(Contract entity) {
+        this.userWorkerId = entity.getWorker().getId();
+        this.jobId = entity.getJob().getId();
+    }
+}
