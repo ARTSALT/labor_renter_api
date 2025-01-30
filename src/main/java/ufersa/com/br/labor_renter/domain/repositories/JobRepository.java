e5b742b3101950ee45459a07b552789cf9b5b7a4
Ricardo Marinho <112431639+ricardkk@users.noreply.github.com>
2025-01-30 19:46:17 -0300

    Update JobRepository.java

--- src/main/java/ufersa/com/br/labor_renter/domain/repositories/JobRepository.java
+++ src/main/java/ufersa/com/br/labor_renter/domain/repositories/JobRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ufersa.com.br.labor_renter.domain.entities.Job;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+  List<Job> findByDescriptionContainingIgnoreCase(String description);
 }
